ef4aeaf45797742a0db4968f3fe93f97cb74abb3
Centro de Desarrollo De Software <cds@univo.edu.sv>
2019-10-21 09:20:26 -0600

    esdsdsdsd

---
+++ InventarioApp/InventarioApp/Models/Repository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventarioApp.Models
+{
+    public class Repository
+    {
+    }
+}
---
+++ InventarioApp/InventarioApp/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,42 @@
+﻿using InventarioApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace InventarioApp.ViewModels
+{
+   public  class CategoryViewModel
+    {
+        #region Propiedades
+        public string CategoryName { get; set; }
+        public string Desciption { get; set; } 
+        public List<Product> Product { get; set; }
+
+        public Command AddNewCategoryCommand { get; set; }
+        public Command AddProductToCategoryCommand { get; set; }
+        private readonly Repository repository;
+        #endregion
+
+        #region Constructor
+        public CategoryViewModel(Repository repository)
+        {
+            this.repository = repository;
+            AddNewCategoryCommand = new Command(SaveCategory);
+            AddProductToCategoryCommand = new Command(AddProduct);
+        }
+
+        private void AddProduct()
+        {
+            
+        }
+
+        private void SaveCategory()
+        {
+            
+        }
+        #endregion
+
+    }
+}
